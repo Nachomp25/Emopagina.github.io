f1b9c95f13a25a6f6b8e77fdf7d2a40215317d87
Nachomp25 <ignaciom1999@hotmail.com>
2024-05-21 23:09:34 -0300

    Add files via upload

---
+++ ADP/scripts.js
@@ -0,0 +1,6 @@
+// Por ahora no hay funcionalidades de JavaScript específicas.
+// Puedes agregar cualquier funcionalidad que desees aquí.
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Formulario enviado. Gracias por contactarme.');
+});
